43de08c4b83f6fd0bce4a10fabee153988fca75d
neemchand <neemchand@ucreate.co.in>
2019-01-10 15:14:33 +0530

    changes in blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 123
+                    Laravel thorugh circle CI
                 </div>
 
                 <div class="links">
