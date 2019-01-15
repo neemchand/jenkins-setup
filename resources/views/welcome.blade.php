706b9b85d2e7132c6cb226db9aae0f0643688ee8
neemchand <neemchand@ucreate.co.in>
2019-01-15 10:39:04 +0530

    text changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel thorugh circle CI
+                    Laravel test app
                 </div>
 
                 <div class="links">
