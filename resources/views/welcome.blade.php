2be846f1aadc21662957a5ab36afcb96838a81c2
neemchand <neemchand@ucreate.co.in>
2019-01-15 15:02:54 +0530

    rollbar test changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel jenkin 3245 as</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,6 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
+                    @php echo $rest;  @endphp
                     Laravel chaosmonkey app test
                 </div>
 
